5e094d9760de19e99f1f8bd95de53bd0ff5cd7bc
slfan1989 <slfan1989@apache.org>
2025-01-25 17:35:36 +0800

    MAPREDUCE-7414. Upgrade Junit 4 to 5 in hadoop-mapreduce-client-hs.

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-hs/src/test/java/org/apache/hadoop/mapreduce/v2/hs/TestJobListCache.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-hs/src/test/java/org/apache/hadoop/mapreduce/v2/hs/TestJobListCache.java
@@ -24,14 +24,16 @@ import org.apache.hadoop.mapreduce.v2.api.records.JobId;
 import org.apache.hadoop.mapreduce.v2.hs.HistoryFileManager.HistoryFileInfo;
 import org.apache.hadoop.mapreduce.v2.hs.HistoryFileManager.JobListCache;
 import org.apache.hadoop.mapreduce.v2.util.MRBuilderUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestJobListCache {
 
-  @Test (timeout = 5000)
+  @Test
+  @Timeout(value = 5)
   public void testAddExisting() {
     JobListCache cache = new JobListCache(2, 1000);
 
@@ -41,11 +43,11 @@ public class TestJobListCache {
 
     cache.addIfAbsent(fileInfo);
     cache.addIfAbsent(fileInfo);
-    assertEquals("Incorrect number of cache entries", 1,
-        cache.values().size());
+    assertEquals(1, cache.values().size(), "Incorrect number of cache entries");
   }
 
-  @Test (timeout = 5000)
+  @Test
+  @Timeout(value = 5)
   public void testEviction() throws InterruptedException {
     int maxSize = 2;
     JobListCache cache = new JobListCache(maxSize, 1000);
@@ -72,8 +74,8 @@ public class TestJobListCache {
       if (values.size() > maxSize) {
         Thread.sleep(100);
       } else {
-        assertFalse("fileInfo1 should have been evicted",
-          values.contains(fileInfo1));
+        assertFalse(
+         values.contains(fileInfo1), "fileInfo1 should have been evicted");
         return;
       }
     }
